d596dd8c8f63430ddfc711c1ff9e9131a0570eb5
burtbeckwith <burtbeckwith@832c1c66-4827-0410-8465-ccb17913cd09>
2010-07-17 07:01:42 +0000

    0.1.1 release
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-app-info/trunk@63007 832c1c66-4827-0410-8465-ccb17913cd09

--- AppInfoGrailsPlugin.groovy
+++ AppInfoGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import org.codehaus.groovy.grails.commons.ConfigurationHolder as CH
 
 class AppInfoGrailsPlugin {
 
-	String version = '0.1'
+	String version = '0.1.1'
 	String grailsVersion = '1.1 > *'
 	def dependsOn = ['dynamicController': '0.2 > *',
 	                 'googleVisualization': '0.2.1 > *',
@@ -11,7 +11,7 @@ class AppInfoGrailsPlugin {
 
 	String author = 'Burt Beckwith'
 	String authorEmail = 'burt@burtbeckwith.com'
-	String title = 'Management plugin'
+	String title = 'Application Info'
 	String description = "UI for inspecting various aspects of the application's configuration"
 	String documentation = 'http://grails.org/plugin/app-info'
 
